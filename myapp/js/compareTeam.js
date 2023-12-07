31ad30564fbc82d6ac7d490bf3e9c5f1f2709257
Jorge <murijo02@luther.edu>
2023-12-07 08:08:26 -0600

    hi

--- myapp/js/compareTeam.js
+++ myapp/js/compareTeam.js
@@ -1,45 +1,60 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const selectedTeams = JSON.parse(localStorage.getItem('selectedTeams'));
-    const team1Name = selectedTeams[0];
-    const team2Name = selectedTeams[1];
+  const selectedTeams = JSON.parse(localStorage.getItem('selectedTeams'));
+  const team1Name = selectedTeams[0];
+  const team2Name = selectedTeams[1];
 
-    // Fetch team data based on IDs
-    fetch(`http://127.0.0.1:5000/api/v1/teams/${team1Name}`)
+  
+  fetch(`http://127.0.0.1:5000/api/v1/teams/${team1Name}`)
+    .then(response => response.json())
+    .then(team1 => {
+      fetch(`http://127.0.0.1:5000/api/v1/teams/${team2Name}`)
         .then(response => response.json())
-        .then(team1 => {
-            fetch(`http://127.0.0.1:5000/api/v1/teams/${team2Name}`)
-                .then(response => response.json())
-                .then(team2 => {
-                    displayTeams(team1, team2);
-                    displayTeamTable(team1, team2);
-                    // Fetch and display initial chart (default field: "fieldGoals")
-                    fetchAndDisplayChart("fieldGoals", team1Name, team2Name);
-                });
-        });
+        .then(team2 => {
+          displayTeams(team1, team2);
+          displayTeamTable(team1, team2);
 
-    // Event listener to change the comparison field
-    document.getElementById('compare-field').addEventListener('change', function () {
-        const selectedField = this.value;
-        // Fetch and update the chart based on the selected field
-        fetchAndDisplayChart(selectedField, team1Name, team2Name);
+          // Fetch and display initial chart (default field: "fieldGoals")
+          fetchAndDisplayChart("fieldGoals", team1, team2);
+        });
     });
 
-    // Function to display team names
-    function displayTeams(team1, team2) {
-        const teamNamesContainer = document.getElementById('team-names');
-        teamNamesContainer.innerHTML = `
-            <div>${team1.name}</div>
-            <div>${team2.name}</div>
-        `;
-    }
+    compareField.addEventListener('change', function () {
+      const selectedField = this.value;
+      const selectedTeams = JSON.parse(localStorage.getItem('selectedTeams'));
+    
+      if (!selectedField) {
+        console.error('Selected field is undefined!');
+        return;
+      }
+    
+      fetchAndDisplayChart(selectedField, selectedTeams[0], selectedTeams[1]);
+    });
 
-    // Function to display team information on the table
-    // Function to display team information on the table
-function displayTeamTable(team1, team2) {
+  // Event listener to change the comparison field
+  const compareField = document.getElementById('compare-field');
+  compareField.addEventListener('change', function () {
+    const selectedField = this.value;
+    if (!selectedField) {
+      console.error('Selected field is undefined!');
+      return;
+    }
+    fetchAndDisplayChart(selectedField, team1, team2);
+  });
+
+  // Function to display team names
+  function displayTeams(team1, team2) {
+    const teamNamesContainer = document.getElementById('team-names');
+    teamNamesContainer.innerHTML = `
+      <div>${team1.name}</div>
+      <div>${team2.name}</div>
+    `;
+  }
+
+  // Function to display team information in the table
+  function displayTeamTable(team1, team2) {
     const teamTableBody = document.getElementById('teamTableBody');
-    // Clear existing rows
     teamTableBody.innerHTML = '';
 
     // Create a row for Team 1
@@ -47,7 +62,7 @@ function displayTeamTable(team1, team2) {
     team1Row.innerHTML = `<td>${team1.name}</td>`;
     const fields = ['location', 'fieldGoals', 'threePointPercent', 'freeThrowPercent', 'rebounds', 'assists', 'steals', 'blocks', 'personalFouls', 'points'];
     fields.forEach(field => {
-        team1Row.innerHTML += `<td>${team1[field]}</td>`;
+      team1Row.innerHTML += `<td>${team1[field]}</td>`;
     });
     teamTableBody.appendChild(team1Row);
 
@@ -55,53 +70,52 @@ function displayTeamTable(team1, team2) {
     const team2Row = document.createElement('tr');
     team2Row.innerHTML = `<td>${team2.name}</td>`;
     fields.forEach(field => {
-        team2Row.innerHTML += `<td>${team2[field]}</td>`;
+      team2Row.innerHTML += `<td>${team2[field]}</td>`;
     });
     teamTableBody.appendChild(team2Row);
-}
+  }
 
-
-    // Function to fetch data and display chart
-    function fetchAndDisplayChart(selectedField, team1Name, team2Name) {
-        // Fetch data for the selected field for both teams
-        fetch(`http://127.0.0.1:5000/api/v1/teams/${team1Name}/${selectedField}`)
-            .then(response => response.json())
-            .then(dataTeam1 => {
-                fetch(`http://127.0.0.1:5000/api/v1/teams/${team2Name}/${selectedField}`)
-                    .then(response => response.json())
-                    .then(dataTeam2 => {
-                        // Get data for the chart
-                        const xValues = [team1Name.name, team2Name.name];
-                        const yValues = [dataTeam1.value, dataTeam2.value];
-                        const barColors = ["red", "green"];
-
-                        // Call the function to create or update the chart
-                        createOrUpdateChart(xValues, yValues, barColors);
-                    });
-            });
+  async function fetchAndDisplayChart(selectedField, team1, team2) {
+    const team1Name = team1.name; // Capture team1.name before the fetch
+    const team2Name = team2.name; // Capture team2.name before the fetch
+  
+    try {
+      const [dataTeam1, dataTeam2] = await Promise.all([
+        fetch(`http://127.0.0.1:5000/api/v1/teams/${team1Name}/${selectedField}`).then(response => response.json()),
+        fetch(`http://127.0.0.1:5000/api/v1/teams/${team2Name}/${selectedField}`).then(response => response.json())
+      ]);
+  
+      const xValues = [team1Name, team2Name];
+      const yValues = [dataTeam1.value, dataTeam2.value];
+      const barColors = ["red", "green"];
+  
+      createOrUpdateChart(xValues, yValues, barColors);
+    } catch (error) {
+      console.error(`Error fetching data: ${error}`);
     }
+  }
+  
 
-    // Function to create or update the chart
-    function createOrUpdateChart(xValues, yValues, barColors) {
-        const chartContainer = document.getElementById('chart-container');
-        // Remove any existing chart
-        chartContainer.innerHTML = '<canvas id="myChart"></canvas>';
+  function createOrUpdateChart(xValues, yValues, barColors) {
+    const chartContainer = document.getElementById('chart-container');
+    chartContainer.innerHTML = '<canvas id="myChart"></canvas>';
 
-        // Create a new chart
-        new Chart("myChart", {
-            type: "bar",
-            data: {
-                labels: xValues,
-                datasets: [{
-                    backgroundColor: barColors,
-                    data: yValues
-                }]
-            },
-            options: {
-                // Customize options as needed
-            }
-        });
-    }
+    new Chart("myChart", {
+      type: "bar",
+      data: {
+        labels: xValues,
+        datasets: [{
+          backgroundColor: barColors,
+          data: yValues
+        }]
+      },
+      options: {
+        // Customize options as needed
+      }
+    });
+  }
 });
+
+
 
 
