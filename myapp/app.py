9bdfda89017bfa9bc15ed32b04ae5bef6f49c6a3
IASWC <123434102+melejo01@users.noreply.github.com>
2023-12-07 04:06:50 -0600

    conference

--- myapp/app.py
+++ myapp/app.py
@@ -18,116 +18,48 @@ with app.app_context():
     teams_schema = TeamSchema(many=True)
 
 
-@app.route("/api/v1/players")
-def get_players():
-    players = Player.query.all()
-    players_data = [
-        {
-            "player": player.player,
-            "teamAbbreviation": player.teamAbbreviation,
-            "games": player.games,
-            "fieldGoals": player.fieldGoals,
-            "threePointPercent": player.threePointPercent,
-            "freeThrowPercent": player.freeThrowPercent,
-            "rebounds": player.rebounds,
-            "assists": player.assists,
-            "steals": player.steals,
-            "blocks": player.blocks,
-            "personalFouls": player.personalFouls,
-            "points": player.points,
-        }
-        for player in players
-    ]
-    return jsonify({"players": players_schema.dump(players_data)})
+@app.route("/api/v1/teams/compare")
+def get_teams_comparison():
+    team1Name = request.args.get("team1Name")
+    team2Name = request.args.get("team2Name")
+    selectedField = request.args.get("selectedField")
+    print(f"Received request for {team1Name}, {team2Name}, {selectedField}")
 
-
-@app.route("/api/v1/teams")
-def get_teams():
-    teams = Team.query.all()
-    teams_data = [
-        {
-            "teamAbbreviation": team.teamAbbreviation,
-            "name": team.name,
-            "location": team.location,
-            "fieldGoals": team.fieldGoals,
-            "threePointPercent": team.threePointPercent,
-            "freeThrowPercent": team.freeThrowPercent,
-            "rebounds": team.rebounds,
-            "assists": team.assists,
-            "steals": team.steals,
-            "blocks": team.blocks,
-            "personalFouls": team.personalFouls,
-            "points": team.points,
-        }
-        for team in teams
-    ]
-    return jsonify({"teams": teams_schema.dump(teams_data)})
-
-
-@app.route("/api/v1/teams/<teamName>")
-def get_team(teamName):
     try:
-        team = Team.query.filter_by(name=teamName).first()
-        if team:
-            team_data = {
-                "teamAbbreviation": team.teamAbbreviation,
-                "name": team.name,
-                "location": team.location,
-                "fieldGoals": team.fieldGoals,
-                "threePointPercent": team.threePointPercent,
-                "freeThrowPercent": team.freeThrowPercent,
-                "rebounds": team.rebounds,
-                "assists": team.assists,
-                "steals": team.steals,
-                "blocks": team.blocks,
-                "personalFouls": team.personalFouls,
-                "points": team.points,
-            }
-            return jsonify(team_data)
-        else:
-            return jsonify({"error": "Team not found."}), 404
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        team1 = Team.query.filter_by(name=team1Name).first()
+        team2 = Team.query.filter_by(name=team2Name).first()
 
+        if team1 and team2:
+            selected_field_value_team1 = getattr(team1, selectedField)
+            selected_field_value_team2 = getattr(team2, selectedField)
 
-@app.route("/api/v1/players/<player>")
-def get_player(player):
-    try:
-        player = Player.query.filter_by(player=player).first()
-        if player:
-            player_data = {
-                "player": player.player,
-                "teamAbbreviation": player.teamAbbreviation,
-                "games": player.games,
-                "fieldGoals": player.fieldGoals,
-                "threePointPercent": player.threePointPercent,
-                "freeThrowPercent": player.freeThrowPercent,
-                "rebounds": player.rebounds,
-                "assists": player.assists,
-                "steals": player.steals,
-                "blocks": player.blocks,
-                "personalFouls": player.personalFouls,
-                "points": player.points,
-            }
-            return jsonify(player_data)
-        else:
-            return jsonify({"error": "Player not found."}), 404
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-@app.route("/api/v1/teams/<teamName>/<selectedField>")
-def get_team_field(team1Name, selectedField):
-    try:
-        team = Team.query.filter_by(name=team1Name).first()
-        if team:
-            selected_field_value = getattr(team, selectedField)
-            if selected_field_value is not None:
-                return jsonify({"field": selectedField, "value": selected_field_value})
+            if (
+                selected_field_value_team1 is not None
+                and selected_field_value_team2 is not None
+            ):
+                return jsonify(
+                    {
+                        "team1": {
+                            "field": selectedField,
+                            "value": selected_field_value_team1,
+                        },
+                        "team2": {
+                            "field": selectedField,
+                            "value": selected_field_value_team2,
+                        },
+                    }
+                )
             else:
-                return jsonify({"error": f"Field '{selectedField}' not found."}), 404
+                return (
+                    jsonify(
+                        {
+                            "error": f"Field '{selectedField}' not found for one or more teams."
+                        }
+                    ),
+                    404,
+                )
         else:
-            return jsonify({"error": "Field not found."}), 404
+            return jsonify({"error": "One or more teams not found."}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
--- myapp/js/compareTeam.js
+++ myapp/js/compareTeam.js
@@ -1,31 +1,55 @@
-// compareTeam.js
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Get team IDs from URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const team1Name = urlParams.get('team1');
-    const team2Name = urlParams.get('team2');
+    // Get team names from localStorage
+    const team1Name = localStorage.getItem('team1');
+    const team2Name = localStorage.getItem('team2');
 
-    // Fetch team data based on IDs
-    fetch(`http://127.0.0.1:5000/api/v1/teams/${team1Name}`)
-        .then(response => response.json())
-        .then(team1 => {
-            fetch(`http://127.0.0.1:5000/api/v1/teams/${team2Name}`)
-                .then(response => response.json())
-                .then(team2 => {
-                    displayTeams(team1, team2);
-                    displayTeamTable(team1, team2);
-                    // Fetch and display initial chart (default field: "fieldGoals")
-                    fetchAndDisplayChart("fieldGoals", team1Name, team2Name);
-                });
+    // Check if team names are available in localStorage
+    if (team1Name && team2Name) {
+        // Fetch team data based on names
+        fetchTeamComparison(team1Name, team2Name);
+
+        // Event listener for changing the comparison field
+        document.getElementById('compare-field').addEventListener('change', function () {
+            const selectedField = this.value;
+            // Fetch and update the chart based on the selected field
+            fetchAndDisplayChart(selectedField, team1Name, team2Name);
         });
+    } else {
+        console.error('Team names not found in localStorage. Fetching from API.');
+        // If team names are not found in localStorage, fetch them from the API
+        // and save them to localStorage for future use
+        fetch('http://127.0.0.1:5000/api/v1/teams/compare?team1Name=Team1&team2Name=Team2')
+            .then(response => response.json())
+            .then(data => {
+                if (data.team1 && data.team2) {
+                    const team1 = data.team1.name;
+                    const team2 = data.team2.name;
+                    localStorage.setItem('team1', team1);
+                    localStorage.setItem('team2', team2);
+                    fetchTeamComparison(team1, team2);
+                } else {
+                    console.error('Error fetching team data from API.');
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching team data from API:', error);
+            });
+    }
 
-    // Event listener for changing the comparison field
-    document.getElementById('compare-field').addEventListener('change', function () {
-        const selectedField = this.value;
-        // Fetch and update the chart based on the selected field
-        fetchAndDisplayChart(selectedField, team1Name, team2Name);
-    });
+    // Function to fetch team comparison data and display initial chart
+    function fetchTeamComparison(team1Name, team2Name) {
+        const selectedField = "fieldGoals";  // Default field for initial chart
+        fetch(`http://127.0.0.1:5000/api/v1/teams/compare?team1Name=${team1Name}&team2Name=${team2Name}&selectedField=${selectedField}`)
+            .then(response => response.json())
+            .then(data => {
+                // Display team names, table, and initial chart
+                displayTeams(data.team1, data.team2);
+                displayTeamTable(data.team1, data.team2);
+                fetchAndDisplayChart(selectedField, team1Name, team2Name);
+            });
+    }
+
+
 
     // Function to display team names
     function displayTeams(team1, team2) {
@@ -43,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
         teamTableBody.innerHTML = '';
 
         // Add rows for each field
-        const fields = ['id', 'abbreviation', 'name', 'location', 'fieldGoals', 'threePointPercent', 'freeThrowPercent', 'rebounds', 'assists', 'steals', 'blocks', 'personalFouls', 'points'];
+        const fields = ['abbreviation', 'name', 'location', 'fieldGoals', 'threePointPercent', 'freeThrowPercent', 'rebounds', 'assists', 'steals', 'blocks', 'personalFouls', 'points'];
         fields.forEach(field => {
             const row = document.createElement('tr');
             row.innerHTML = `
@@ -61,11 +85,11 @@ document.addEventListener('DOMContentLoaded', function () {
         fetch(`http://127.0.0.1:5000/api/v1/teams/${team1Name}/${selectedField}`)
             .then(response => response.json())
             .then(dataTeam1 => {
-                fetch(`/api/teams/${team2NAme}/${selectedField}`)
+                fetch(`http://127.0.0.1:5000/api/v1/teams/${team2Name}/${selectedField}`)
                     .then(response => response.json())
                     .then(dataTeam2 => {
                         // Get data for the chart
-                        const xValues = [team1.name, team2.name];
+                        const xValues = [team1Name, team2Name];
                         const yValues = [dataTeam1.value, dataTeam2.value];
                         const barColors = ["red", "green"];
 
@@ -80,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const chartContainer = document.getElementById('chart-container');
         // Remove any existing chart
         chartContainer.innerHTML = '<canvas id="myChart"></canvas>';
-        
+
         // Create a new chart
         new Chart("myChart", {
             type: "bar",
@@ -97,4 +121,3 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
-
